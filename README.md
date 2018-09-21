8dac8bcc0fc4236fdc4e1928f6a6968e2827e5c5
GoForBroke <go.for.broke1006@gmail.com>
2018-09-21 22:45:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# channel-debug-cpp
+
+Golang-style multi-threading structure realization for C++.
---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <condition_variable>
+#include <queue>
+#include <unistd.h>
+
+using namespace std;
+
+template<class T>
+class Channel {
+private:
+    std::queue<T> buffer;
+    size_t capacity;
+    mutex buffer_mutex;
+    mutex wait_not_empty_mutex;
+    condition_variable cv;
+public:
+    explicit Channel(size_t c) : capacity(c) {}
+
+    void operator<<(const T obj) {
+        while (buffer.size() == capacity)
+            std::this_thread::yield();
+        while (!buffer_mutex.try_lock())
+            std::this_thread::yield();
+        buffer.push(obj);
+        buffer_mutex.unlock();
+    }
+
+    void operator>>(T &obj) {
+        while (buffer.empty())
+            std::this_thread::yield();
+        unique_lock<mutex> lock(buffer_mutex);
+        obj = buffer.front();
+        buffer.pop();
+    }
+};
+
+void writeRoutine(Channel<string> &ch, __useconds_t delay, int index) {
+    while (true) {
+        ch << ("hello " + std::to_string(index));
+        usleep(delay);
+    }
+}
+
+void readRoutine(Channel<string> &ch) {
+    string s;
+    while (true) {
+        ch >> s;
+        cout << s << endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    Channel<string> ch(5);
+
+    std::vector<thread> tasks;
+
+    tasks.emplace_back(writeRoutine, std::ref(ch), 500, 1);
+    tasks.emplace_back(writeRoutine, std::ref(ch), 300, 2);
+    tasks.emplace_back(writeRoutine, std::ref(ch), 250, 3);
+
+    tasks.emplace_back(readRoutine, std::ref(ch));
+
+    for (thread &t : tasks)
+        t.join();
+
+    return 0;
+}
