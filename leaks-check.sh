2a6ced4b6a088a382b8e7c9eeccb35a1a007a542
GoForBroke <go.for.broke1006@gmail.com>
2018-09-25 01:16:10 +0300

    Leaks check

--- leaks-check.sh
+++ leaks-check.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+DATE=`date '+%Y-%m-%d-%H:%M:%S'`
+
 mkdir -p ./leaks-check/
-g++ --std=c++11 -o ./leaks-check/channel_debug channel.h main.cpp -lpthread
+g++ --std=c++11 -O2 -o ./leaks-check/channel_debug channel.h main.cpp -lpthread
+
 valgrind --leak-check=full \
          --show-leak-kinds=all \
          --track-origins=yes \
          --verbose \
-         --log-file=./leaks-check/report.log \
+         --log-file=./leaks-check/leaks-${DATE}.log \
          ./leaks-check/channel_debug
+
+valgrind --tool=massif --stacks=yes --massif-out-file=./leaks-check/massif.out ./leaks-check/channel_debug
+ms_print ./leaks-check/massif.out > "./leaks-check/ram-usage-${DATE}.log"
