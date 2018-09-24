9356f7869b2490fc7f1090b511b856082284191f
GoForBroke <go.for.broke1006@gmail.com>
2018-09-25 00:20:16 +0300

    Realize channel class

---
+++ channel.h
@@ -0,0 +1,62 @@
+//
+// Created by goforbroke on 24.09.18.
+//
+
+#ifndef CHANNEL_DEBUG_CHANNEL_H
+#define CHANNEL_DEBUG_CHANNEL_H
+
+#include <queue>
+#include <condition_variable>
+
+using namespace std;
+
+template<class T>
+class Channel {
+private:
+    size_t capacity;
+    queue<T> buffer;
+    mutex buffer_mutex;
+    condition_variable cv_empty;
+    condition_variable cv_full;
+public:
+    explicit Channel(size_t c) : capacity(c) {}
+
+    /**
+     * write to channel
+     */
+    void operator<<(const T obj) {
+        unique_lock<mutex> lock(buffer_mutex);
+        cv_full.wait(lock, [this] { return getLength() < getCapacity(); });
+        buffer.push(obj);
+        cv_empty.notify_one();
+    }
+
+    /**
+     * read from channel
+     */
+    void operator>>(T &obj) {
+        unique_lock<mutex> lock(buffer_mutex);
+        cv_empty.wait(lock, [this] { return getLength() > 0; });
+        obj = buffer.front();
+        buffer.pop();
+        cv_full.notify_one();
+    }
+
+    const size_t getLength() const {
+        return buffer.size();
+    }
+
+    const size_t &getCapacity() const {
+        return capacity;
+    }
+
+    const bool isFull() const {
+        return getLength() == getCapacity();
+    }
+
+    const bool isEmpty() const {
+        return getLength() == 0;
+    }
+};
+
+#endif //CHANNEL_DEBUG_CHANNEL_H
--- main.cpp
+++ main.cpp
@@ -1,62 +1,12 @@
 #include <iostream>
-#include <condition_variable>
 #include <thread>
-#include <mutex>
-#include <queue>
 #include <unistd.h>
+
+#include "channel.h"
 
 using namespace std;
 
 #define SECOND_SLEEP 1000000
-
-template<class T>
-class Channel {
-private:
-    std::queue<T> buffer;
-    size_t capacity;
-    mutex buffer_mutex;
-    std::condition_variable cve;
-    std::condition_variable cvf;
-public:
-    explicit Channel(size_t c) : capacity(c) {}
-
-    /**
-     * write to channel
-     */
-    void operator<<(const T obj) {
-        unique_lock<mutex> lock(buffer_mutex);
-        cvf.wait(lock, [this] { return getLength() < getCapacity(); });
-        buffer.push(obj);
-        cve.notify_one();
-    }
-
-    /**
-     * read from channel
-     */
-    void operator>>(T &obj) {
-        unique_lock<mutex> lock(buffer_mutex);
-        cve.wait(lock, [this] { return getLength() > 0; });
-        obj = buffer.front();
-        buffer.pop();
-        cvf.notify_one();
-    }
-
-    const size_t getLength() const {
-        return buffer.size();
-    }
-
-    const size_t &getCapacity() const {
-        return capacity;
-    }
-
-    const bool isFull() const {
-        return getLength() == getCapacity();
-    }
-
-    const bool isEmpty() const {
-        return getLength() == 0;
-    }
-};
 
 void writeRoutine(Channel<string> &ch, __useconds_t delay, int index, unsigned int fakeValuesCount) {
     for (int i = 0; i < fakeValuesCount; ++i) {
