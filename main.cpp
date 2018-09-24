31ae4a38d631e02a77952cc596b8ca1c14f75cd6
Sergey <go.for.broke1006@gmail.com>
2018-09-24 15:54:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <condition_variable>
 #include <thread>
 #include <mutex>
 #include <queue>
@@ -14,23 +15,21 @@ private:
     std::queue<T> buffer;
     size_t capacity;
     mutex buffer_mutex;
+    std::condition_variable cv;
 public:
     explicit Channel(size_t c) : capacity(c) {}
 
     void operator<<(const T obj) {
-        while (buffer.size() >= capacity) {
-            std::this_thread::yield();
-        }
-        std::unique_lock<mutex> lock(buffer_mutex);
+        unique_lock<mutex> lock(buffer_mutex);
+        cv.wait(lock, [this]{ return getLength() < getCapacity(); });
         buffer.push(obj);
     }
 
     void operator>>(T &obj) {
-        while (buffer.empty() || !buffer_mutex.try_lock() || buffer.empty())
-            std::this_thread::yield();
+        unique_lock<mutex> lock(buffer_mutex);
         obj = buffer.front();
         buffer.pop();
-        buffer_mutex.unlock();
+        cv.notify_one();
     }
 
     const size_t getLength() const {
@@ -44,7 +43,7 @@ public:
 
 void writeRoutine(Channel<string> &ch, __useconds_t delay, int index) {
     while (true) {
-        ch << ("hello " + std::to_string(index));
+        ch << ("hello " + to_string(index));
         usleep(delay);
     }
 }
@@ -61,13 +60,13 @@ void readRoutine(Channel<string> &ch) {
 int main(int argc, char **argv) {
     Channel<string> ch(5);
 
-    std::vector<thread> tasks;
+    vector<thread> tasks;
 
-    tasks.emplace_back(writeRoutine, std::ref(ch), 500, 1);
-    tasks.emplace_back(writeRoutine, std::ref(ch), 300, 2);
-    tasks.emplace_back(writeRoutine, std::ref(ch), 250, 3);
+    tasks.emplace_back(writeRoutine, ref(ch), 500, 1);
+    tasks.emplace_back(writeRoutine, ref(ch), 300, 2);
+    tasks.emplace_back(writeRoutine, ref(ch), 250, 3);
 
-    tasks.emplace_back(readRoutine, std::ref(ch));
+    tasks.emplace_back(readRoutine, ref(ch));
 
     for (thread &t : tasks)
         t.join();
