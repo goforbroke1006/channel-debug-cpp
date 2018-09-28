a273d45edd9a88f1b24776e210b306667d2b66e7
GoForBroke <go.for.broke1006@gmail.com>
2018-09-28 10:07:20 +0300

    Tests

--- leaks-check.sh
+++ leaks-check.sh
@@ -6,11 +6,14 @@ mkdir -p ./leaks-check/
 g++ --std=c++11 -O2 -o ./leaks-check/channel_debug channel.h main.cpp -lpthread
 
 valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=./leaks-check/leaks-${DATE}.log \
-         ./leaks-check/channel_debug
+    --show-leak-kinds=all \
+    --track-origins=yes \
+    --verbose \
+    --log-file=./leaks-check/leaks-${DATE}.log \
+    ./leaks-check/channel_debug
 
-valgrind --tool=massif --stacks=yes --massif-out-file=./leaks-check/massif.out ./leaks-check/channel_debug
+valgrind --tool=massif \
+    --stacks=yes --heap=yes \
+    --massif-out-file=./leaks-check/massif.out \
+    ./leaks-check/channel_debug
 ms_print ./leaks-check/massif.out > "./leaks-check/ram-usage-${DATE}.log"
--- main.cpp
+++ main.cpp
@@ -18,33 +18,27 @@ void writeRoutine(Channel<string> &ch, __useconds_t delay, int index, unsigned i
 void readRoutine(Channel<string> &ch, unsigned int waitJobsCount) {
     string s;
     for (int i = 0; i < waitJobsCount; ++i) {
-        cout << ch.getLength();
-        cout << " (" << (ch.isFull() ? "full" : "not full") << ") ";
         ch >> s;
-        cout << " : ";
-        cout << s;
-        cout << " : " << ch.getLength() << " : " << ch.getCapacity();
-        cout << endl;
-        usleep(0.5 * SECOND_SLEEP);
+        cout << s << endl;
     }
 }
 
 int main(int argc, char **argv) {
-    Channel<string> ch(5);
+    Channel<string> ch(50000);
 
     vector<thread> tasks;
 
-    int limit1 = 8;
-    int limit2 = 3;
-    int limit3 = 2;
-    int limit4 = 9;
-    int limit5 = 3;
+    int limit1 = 8000;
+    int limit2 = 3000;
+    int limit3 = 2000;
+    int limit4 = 9000;
+    int limit5 = 3000;
 
-    tasks.emplace_back(writeRoutine, ref(ch), 500, 1, limit1);
-    tasks.emplace_back(writeRoutine, ref(ch), 300, 2, limit2);
-    tasks.emplace_back(writeRoutine, ref(ch), 250, 3, limit3);
-    tasks.emplace_back(writeRoutine, ref(ch), 450, 4, limit4);
-    tasks.emplace_back(writeRoutine, ref(ch), 850, 5, limit5);
+    tasks.emplace_back(writeRoutine, ref(ch), /*5*/0, 1, limit1);
+    tasks.emplace_back(writeRoutine, ref(ch), /*3*/0, 2, limit2);
+    tasks.emplace_back(writeRoutine, ref(ch), /*2*/0, 3, limit3);
+    tasks.emplace_back(writeRoutine, ref(ch), /*4*/0, 4, limit4);
+    tasks.emplace_back(writeRoutine, ref(ch), /*8*/0, 5, limit5);
 
     int bitesCount = limit1 + limit2 + limit3 + limit4 + limit5;
     cout << "Bites count: " << bitesCount << endl;
